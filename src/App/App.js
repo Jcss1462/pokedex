71bfa83d170be5c9f7778976a313bae11d324c66
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2024-07-30 20:22:36 -0500

    Se añade logo a la pokedex

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import { Col } from 'antd'
+import  logo  from '../assets/logo.svg'
 import { Searcher } from '../components/searcher/searcher.jsx';
 import { PokemonList } from '../components/pokemonList/pokemonList.jsx';
 
@@ -8,6 +9,9 @@ import { PokemonList } from '../components/pokemonList/pokemonList.jsx';
 function App() {
   return (
     <div className="App">
+      <Col span={4} offset={10}>
+        <img src={logo} alt="Pokedex"></img>
+      </Col>
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
